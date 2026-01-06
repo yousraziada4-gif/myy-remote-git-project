16b2cadc52a673d4e388aab02fe47bff8a0b3794
yousra <yousraziada4@gmail.com>
2026-01-06 18:16:24 +0100

    initial commit setting up structure

---
+++ src/main/java/com/l3/gl/MyyRemoteGitProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.l3.gl;
+
+/**
+ *
+ * @author hp
+ */
+public class MyyRemoteGitProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
